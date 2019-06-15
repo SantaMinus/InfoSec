8999a0cbdb91df2d18a60a09eb28ff35faef8404
SantaMinus <santaminus@gmail.com>
2019-06-15 21:36:39 +0300

    Create a DB connection

---
+++ src/main/java/com/sava/db/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package com.sava.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConnection.class);
+
+    public void connect() {
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/savadb", "SA", "");
+            if (con != null) {
+                LOGGER.debug("Connection created successfully");
+            } else {
+                LOGGER.error("Problem with creating connection");
+            }
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("JDBCDriver not found", e);
+        } catch (SQLException e) {
+            LOGGER.error("Failed to create a DB connection", e);
+        }
+    }
+}
