43f0360bba3c3af853943b9deaf0421510fbac44
SantaMinus <santaminus@gmail.com>
2019-06-28 23:50:06 +0300

    - make Authenticator polymorphic (XML & DB) - start writing DB auth

--- src/main/java/com/sava/db/DatabaseConnection.java
+++ src/main/java/com/sava/db/DatabaseConnection.java
@@ -10,10 +10,12 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConnection.class);
 
-    public void connect() {
+    public Connection connect() {
+        Connection con = null;
+
         try {
             Class.forName("org.hsqldb.jdbc.JDBCDriver");
-            Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/savadb", "SA", "");
+            con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/savadb", "SA", "");
             if (con != null) {
                 LOGGER.debug("Connection created successfully");
             } else {
@@ -24,5 +26,7 @@ public class DatabaseConnection {
         } catch (SQLException e) {
             LOGGER.error("Failed to create a DB connection", e);
         }
+
+        return con;
     }
 }
