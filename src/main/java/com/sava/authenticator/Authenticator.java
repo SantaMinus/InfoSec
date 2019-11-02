e54d4ec6835ac69b4fa69eced2a3af3e0bc97eae
Katia Savina <santaminus@gmail.com>
2019-11-02 19:56:57 +0200

    Add DB connection (#2)
    * Create a DB connection
    
    * - make Authenticator polymorphic (XML & DB)
    - start writing DB auth
    
    * Use DBAuthenticator, experiment with DBs (connection established, table not found)
    
    * Add the simplest check
    
    * Downgrade h2 to 1.4.196 to avoid https://github.com/h2database/h2database/issues/2204
    
    * Post-merge changes

--- src/main/java/com/sava/authenticator/Authenticator.java
+++ src/main/java/com/sava/authenticator/Authenticator.java
@@ -1,57 +1,7 @@
 package com.sava.authenticator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
-
 import com.sava.exception.AuthenticatorException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-@Component
-public class Authenticator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Authenticator.class);
-    private static final File FILE = new File("access.xml");
-    private static final String FILE_NOT_FOUND_ERROR = "File not found";
-
-    public boolean authenticate(String captcha, String login, char[] password, int c1, int c2) throws AuthenticatorException {
-        int capt = Integer.parseInt(captcha);
-        String[] arr;
-        boolean access = false;
-
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(FILE);
-        } catch (FileNotFoundException e) {
-            LOGGER.error(FILE_NOT_FOUND_ERROR, e);
-        }
-        if (scanner == null) {
-            throw new AuthenticatorException("Failed to create a file scanner");
-        }
-        String s;
-
-        while (scanner.hasNext()) {
-            s = scanner.nextLine();
-            arr = s.split(" ");
-
-            if (login.equals(arr[0]) && Arrays.equals(password, arr[1].toCharArray()) && capt == c1 + c2) {
-                File directory = new File("F:/root/" + login);
-                access = true;
-                if (!directory.exists())
-                    directory.mkdir();
-                try {
-                    Runtime.getRuntime().exec("attrib +H " + "F:/root/" + login);
-                } catch (IOException e) {
-                    LOGGER.error("Failed to execute a command", e);
-                }
-                LOGGER.debug("{} signed in", login);
-            }
-        }
-        scanner.close();
-        return access;
-    }
+public interface Authenticator {
+    boolean authenticate(String captcha, String login, char[] password, int c1, int c2) throws AuthenticatorException;
 }
---
+++ src/main/java/com/sava/authenticator/DBAuthenticator.java
@@ -0,0 +1,36 @@
+package com.sava.authenticator;
+
+import com.sava.db.DatabaseConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Component("authenticator")
+public class DBAuthenticator implements Authenticator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DBAuthenticator.class);
+
+    @Override
+    public boolean authenticate(String captcha, String login, char[] password, int c1, int c2) {
+        DatabaseConnection connection = new DatabaseConnection();
+        Connection con = connection.connect();
+
+        try (Statement stmt = con.createStatement()) {
+            ResultSet res = stmt.executeQuery("SELECT LOGIN, PASSWORD FROM USERS");
+            if (res.next()) {
+                LOGGER.debug("login: {}, pass: {}", res.getString("LOGIN"), res.getString("PASSWORD"));
+                if (res.getString("LOGIN").equals(login)
+                        && res.getString("PASSWORD").equals(String.valueOf(password))) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Failed to execute SQL query", e);
+        }
+        return false;
+    }
+}
