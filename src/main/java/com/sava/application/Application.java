442c2a01f9b10b959fb0068d0423f774035481ea
SantaMinus <santaminus@gmail.com>
2020-01-07 22:40:39 +0200

    Write into a DB using hibernate

--- src/main/java/com/sava/application/Application.java
+++ src/main/java/com/sava/application/Application.java
@@ -1,6 +1,10 @@
 package com.sava.application;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.sava.entity.User;
 import com.sava.ui.MainWindow;
+import org.hibernate.cfg.Configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +17,21 @@ public class Application {
         LOGGER.debug("App started");
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        User user = new User("testUser", "pass");
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
 
         MainWindow mainWindow = context.getBean("mainWindow", MainWindow.class);
         mainWindow.signIn();
 
         context.close();
+        factory.close();
     }
 }
--- src/main/java/com/sava/entity/User.java
+++ src/main/java/com/sava/entity/User.java
@@ -1,15 +1,24 @@
 package com.sava.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_PK")
+    @SequenceGenerator(name = "seq_PK", sequenceName = "SYSTEM_SEQUENCE_1A5127C4_56AA_478D_BFAD_238603ABAC74", allocationSize = 1)
+    @Column(name = "pk", updatable = false, nullable = false)
     private int pk;
+
     private String login;
+
     private String password;
 
     public User() {
